e6d903244d1ab484667354d74751399be37955c6
jaehyeon <joali9807@naver.com>
2019-06-11 16:01:48 +0900

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    
+    text = request.GET['fulltext']
+    words = text.split()
+    word_dictionary={}
+    for word in words:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word]=1
+    context = {
+        'full': text,
+        'total': len(words),
+        'dictionary':word_dictionary.items(),
+
+    }
+    return render(request, 'result.html', context)
